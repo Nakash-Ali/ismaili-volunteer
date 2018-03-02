6b405b0327aa10e4209daf3fb1159779679b2747
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-03-02 08:50:30 -0500

    Bump version to 1.0.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Volunteer.Mixfile do
   def project do
     [
       app: :volunteer,
-      version: "1.0.5",
+      version: "1.0.6",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
