56e55e4cdfff8ffc9fd24667c7a72a302c687dde
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-09-04 09:07:47 -0400

    Fix sessions

--- lib/volunteer_web/session.ex
+++ lib/volunteer_web/session.ex
@@ -2,12 +2,10 @@ defmodule VolunteerWeb.Session do
   import Plug.Conn
   alias VolunteerWeb.Router.Helpers
   alias Volunteer.Accounts
-  require Logger
 
   @mock_sessions_user_id 1
 
   def login(conn, %Accounts.User{} = user) do
-    Logger.warn "Logging in user with id #{user.id}"
     put_session(conn, :current_user_id, user.id)
   end
 
@@ -41,17 +39,13 @@ defmodule VolunteerWeb.Session do
   def get_current_user_id(conn) do
     case Plug.Conn.get_session(conn, :current_user_id) do
       nil ->
-        Logger.warn "Did not find user_id in session, checking for mock"
         if VolunteerWeb.Session.should_mock_sessions?() do
-          Logger.warn "SHOULD MOCK! mocking now"
           @mock_sessions_user_id
         else
-          Logger.warn "No mocks, allowing nil"
           nil
         end
 
       id ->
-        Logger.warn "Found id #{id} in session"
         id
     end
   end
@@ -62,22 +56,19 @@ defmodule VolunteerWeb.Session do
 
   defmodule Plugs do
     def load_current_user(conn, _) do
-      Logger.warn "getting current_user_id from session"
       case VolunteerWeb.Session.get_current_user_id(conn) do
         nil ->
-          Logger.warn "No current_user_id, returning"
           conn
 
         user_id ->
-          Logger.warn "Loading current user with id #{user_id}"
           case Accounts.get_user(user_id) do
             nil ->
-              Logger.warn "Did not find user! Logging out"
-              VolunteerWeb.Session.logout(conn)
+              conn
+              |> VolunteerWeb.Session.logout()
 
             user ->
-              Logger.warn "Found user, putting user in session"
-              VolunteerWeb.Session.put_user(conn, user)
+              conn
+              |> VolunteerWeb.Session.put_user(user)
           end
       end
     end
@@ -85,11 +76,9 @@ defmodule VolunteerWeb.Session do
     def ensure_authenticated(conn, _) do
       case VolunteerWeb.Session.get_user(conn) do
         %Accounts.User{} ->
-          Logger.warn "User exists in session, therefore user is authenticated"
           conn
 
         _ ->
-          Logger.warn "User is not authenticated, redirecting to login page"
           conn
           |> VolunteerWeb.Session.put_redirect()
           |> Phoenix.Controller.put_flash(:error, "Please log in to view this page")
