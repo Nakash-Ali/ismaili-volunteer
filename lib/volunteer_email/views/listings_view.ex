d02581f8ead39e164b8839df301b795c3ba582a9
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-10-18 23:37:31 -0400

    Fix compile warnings

--- lib/volunteer_email/views/listings_view.ex
+++ lib/volunteer_email/views/listings_view.ex
@@ -1,6 +1,5 @@
 defmodule VolunteerEmail.ListingsView do
   use VolunteerEmail, :view
-  alias VolunteerWeb.Router.Helpers
   alias VolunteerWeb.Presenters.Title
   alias VolunteerWeb.ListingView, as: PublicListingView
 end
