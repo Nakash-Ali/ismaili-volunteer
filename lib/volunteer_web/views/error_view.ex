2cf99516a18c5e58d34b5f13185c96b9cfa75a8d
Alizain Feerasta <alizain.feerasta@gmail.com>
2019-01-02 11:41:14 +0500

    Add error pages for 400 and 401 status codes

--- lib/volunteer_web/views/error_view.ex
+++ lib/volunteer_web/views/error_view.ex
@@ -1,9 +1,3 @@
 defmodule VolunteerWeb.ErrorView do
   use VolunteerWeb, :view
-
-  # In case no render clause matches or no
-  # template is found, let's render it as 500
-  def template_not_found(_template, assigns) do
-    render("500.html", assigns)
-  end
 end
