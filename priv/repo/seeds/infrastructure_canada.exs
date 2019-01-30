7f46b4ce9291f7984f45ebd4bfbfe2b016af2b9f
Alizain Feerasta <alizain.feerasta@gmail.com>
2019-01-29 22:13:23 -0500

    Add Council for BC group

--- priv/repo/seeds/infrastructure_canada.exs
+++ priv/repo/seeds/infrastructure_canada.exs
@@ -3,7 +3,7 @@
 canada = Volunteer.Infrastructure.seed_region!(1, "Canada", nil)
 
 ontario = Volunteer.Infrastructure.seed_region!(2, "Ontario", canada)
-_bcolumbia = Volunteer.Infrastructure.seed_region!(3, "British Columbia", "bc", canada)
+british_columbia = Volunteer.Infrastructure.seed_region!(3, "British Columbia", "bc", canada)
 _edmonton = Volunteer.Infrastructure.seed_region!(4, "Edmonton", canada)
 _ottawa = Volunteer.Infrastructure.seed_region!(5, "Ottawa", canada)
 _prairies = Volunteer.Infrastructure.seed_region!(6, "Prairies", canada)
@@ -19,3 +19,5 @@ Volunteer.Infrastructure.seed_group!(4, "Aga Khan Health Board (AKHBO) Ontario",
 Volunteer.Infrastructure.seed_group!(5, "ITREB Ontario", ontario)
 Volunteer.Infrastructure.seed_group!(6, "Economic Planning Board (EPB) Ontario", ontario)
 Volunteer.Infrastructure.seed_group!(7, "Aga Khan Youth & Sports Board (AKYSB) Ontario", ontario)
+
+Volunteer.Infrastructure.seed_group!(8, "Council for British Columbia", british_columbia)
