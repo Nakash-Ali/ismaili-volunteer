a40bc16eff69e5c635cc51d8b30a4c2efa43f8b6
Alizain Feerasta <alizain.feerasta@gmail.com>
2019-03-06 19:17:05 -0500

    Update Prairies

--- lib/volunteer/permissions/hardcoded_roles.ex
+++ lib/volunteer/permissions/hardcoded_roles.ex
@@ -75,6 +75,94 @@ defmodule Volunteer.Permissions.HardcodedRoles do
     16 => %{
       "gulnar.kamadia@iicanada.net" => "admin",
       "nargis.alibhai@iicanada.net" => "admin",
+    },
+    # Legal Prairies
+    18 => %{
+      "zahra.allidina@iicanada.net" => "admin"
+    },
+    # Settlement Prairies
+    19 => %{
+      "aziz.barna@iicanada.net" => "admin"
+    },
+    # Community Relations Prairies
+    20 => %{
+      "tasneem.rahim@iicanada.net" => "admin"
+    },
+    # Resource Mobilization Prairies
+    21 => %{
+      "ayaz.gulamhussein@iicanada.net" => "admin"
+    },
+    # JK Development and Maintenance Prairies
+    22 => %{
+      "riyaz.virani@iicanada.net" => "admin"
+    },
+    # Ismaili Transportation Committee Prairies
+    23 => %{
+      "riyaz.virani@iicanada.net" => "admin"
+    },
+    # Catering Prairies
+    24 => %{
+      "amyn.kanjee@iicanada.net" => "admin"
+    },
+    # Ismaili Volunteers Corp (IVC) Prairies
+    25 => %{
+      "farid.kassam@iicanada.net" => "admin"
+    },
+    # Women's Portfolio Prairies
+    26 => %{
+      "shazia.rahmani@iicanada.net" => "admin"
+    },
+    # Ismaili Volunteers (IV) Prairies
+    27 => %{
+      "ranita.charania@iicanada.net" => "admin"
+    },
+    # Arts & Culture Prairies
+    28 => %{
+      "noor.alizada@iicanada.net" => "admin"
+    },
+    # Audio Visual (AV) Prairies
+    29 => %{
+      "noor.alizada@iicanada.net" => "admin"
+    },
+    # Finance Prairies
+    30 => %{
+      "ali.malek@iicanada.net" => "admin"
+    },
+    # Planning, Priorities & Evaluation Prairies
+    31 => %{
+      "narmin.vasanji@iicanada.net" => "admin"
+    },
+    # Care for the Elderly Prairies
+    32 => %{
+      "arif.manji@iicanada.net" => "admin"
+    },
+    # Social Welfare Board (SWB) Prairies
+    33 => %{
+      "adam.jivani@iicanada.net" => "admin"
+    },
+    # Education Board Prairies
+    34 => %{
+      "hassan.odhwani@iicanada.net" => "admin"
+    },
+    # Youth & Sports Board (AKYSB) Prairies
+    35 => %{
+      "rahim.moledina@iicanada.net" => "admin"
+    },
+    # Health Board Prairies
+    36 => %{
+      "zahra.shajani@iicanada.net" => "admin"
+    },
+    # Economic Planning Board (EPB) Prairies
+    37 => %{
+      "talib.daredia@iicanada.net" => "admin"
+    },
+    # Quality of Life (QoL) Prairies
+    38 => %{
+      "shaheed.moledina@iicanada.net" => "admin"
+    },
+    # Communications Prairies
+    39 => {
+      "sheizana.murji@iicanada.net" => "admin"
     }
   }
 
