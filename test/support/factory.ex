28de6394457fd03ef6e018d81beadec9c59c38ce
Alizain Feerasta <alizain.feerasta@gmail.com>
2019-07-01 12:42:11 +0500

    Fix tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -59,8 +59,7 @@ defmodule Volunteer.TestSupport.Factory do
         start_date: Faker.Date.backward(100),
         end_date: Faker.Date.forward(200),
         time_commitment_amount: Faker.random_between(1, 10),
-        time_commitment_type:
-          Volunteer.Listings.Listing.time_commitment_type_choices() |> Enum.random(),
+        time_commitment_type: Volunteer.Listings.Listing.time_commitment_type_choices() |> Enum.random(),
         program_description: Faker.Lorem.sentences(2..5) |> Enum.join(" "),
         responsibilities: Faker.Lorem.sentences(2..5) |> Enum.join(" "),
         qualifications: Faker.Lorem.sentences(2..5) |> Enum.join(" "),
--- test/volunteer/listings/listings_test.exs
+++ test/volunteer/listings/listings_test.exs
@@ -4,6 +4,12 @@ defmodule Volunteer.ListingsTest do
 
   describe "get_all_listings_for_expiry_reminder/1" do
     test "gets un-expired and un-reminded listings" do
+      fields_to_validate = [
+        :id,
+        :expiry_date,
+        :expiry_reminder_sent
+      ]
+
       expiry_date = Faker.DateTime.forward(8)
 
       unexpired_and_unreminded =
@@ -11,6 +17,7 @@ defmodule Volunteer.ListingsTest do
           expired?: false,
           overrides: %{expiry_reminder_sent: false, expiry_date: expiry_date}
         })
+        |> Map.take(fields_to_validate)
 
       _unexpired_and_reminded =
         Factory.listing!(%{
@@ -33,6 +40,7 @@ defmodule Volunteer.ListingsTest do
                expiry_date
                |> Timex.shift(seconds: 1)
                |> Volunteer.Listings.get_all_listings_for_expiry_reminder()
+               |> Enum.map(&Map.take(&1, fields_to_validate))
     end
   end
 end
