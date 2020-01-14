1cbb03b6ddeabdeafe37b86d8640609bd8b8632f
Alizain Feerasta <alizain.feerasta@gmail.com>
2020-01-13 20:16:36 -0500

    Replace content of old file in new place with new content

--- lib/volunteer_web/controllers/admin/listing/tkn_controller.ex
+++ lib/volunteer_web/controllers/admin/listing/tkn_controller.ex
@@ -1,187 +1,78 @@
-defmodule VolunteerWeb.Admin.TKNListingController do
+defmodule VolunteerWeb.Admin.Listing.TKNController do
   use VolunteerWeb, :controller
+  alias Volunteer.Repo
   alias Volunteer.Listings
   alias VolunteerWeb.FlashHelpers
   import VolunteerWeb.ConnPermissions, only: [authorize: 2]
 
+  @preloads %{
+    show: [],
+    edit: [],
+    update: [],
+  }
+
   # Plugs
 
-  plug :load_tkn_listing
   plug :load_listing
   plug :authorize,
-    action_root: [:admin, :listing, :tkn_listing],
+    action_root: [:admin, :listing, :tkn],
     assigns_subject_key: :listing
-
-  plug :redirect_to_show_if_exists
-       when action in [:new, :create]
-
-  plug :redirect_to_show_if_not_exists
-       when action in [:edit, :update, :delete]
-
-  def load_tkn_listing(%Plug.Conn{params: %{"listing_id" => id}} = conn, _opts) do
-    tkn_listing = Listings.get_one_tkn_listing_for_listing(id)
-    Plug.Conn.assign(conn, :tkn_listing, tkn_listing)
-  end
+  plug :validate
 
   def load_listing(%Plug.Conn{params: %{"listing_id" => id}} = conn, _opts) do
-    listing = Listings.get_one_admin_listing!(id)
+    listing =
+      id
+      |> Listings.get_one_admin!()
+      |> Repo.preload(Map.fetch!(@preloads, action_name(conn)))
+
     Plug.Conn.assign(conn, :listing, listing)
   end
 
-  def redirect_to_show_if_exists(conn, _) do
-    %Plug.Conn{
-      assigns: %{tkn_listing: tkn_listing},
-      params: %{"listing_id" => listing_id}
-    } = conn
-
-    case tkn_listing do
-      nil ->
-        conn
-
-      _ ->
-        conn
-        |> FlashHelpers.put_paragraph_flash(
-          :error,
-          "TKN data already exists, cannot create again! To change, edit instead."
-        )
-        |> redirect(to: RouterHelpers.admin_listing_tkn_listing_path(conn, :show, listing_id))
-    end
-  end
-
-  def redirect_to_show_if_not_exists(conn, _) do
-    %Plug.Conn{
-      assigns: %{tkn_listing: tkn_listing},
-      params: %{"listing_id" => listing_id}
-    } = conn
-
-    case tkn_listing do
-      nil ->
-        conn
-        |> FlashHelpers.put_paragraph_flash(:error, "TKN data does not exist, you must create it first!")
-        |> redirect(to: RouterHelpers.admin_listing_tkn_listing_path(conn, :show, listing_id))
-
-      _ ->
-        conn
-    end
+  def validate(%Plug.Conn{assigns: %{listing: listing}} = conn, _) do
+    Plug.Conn.assign(
+      conn,
+      :valid?,
+      Listings.TKN.Introspect.valid?(listing)
+    )
   end
 
   # Controller Actions
 
   def show(conn, _params) do
-    %Plug.Conn{assigns: %{listing: listing}} = conn
+    %Plug.Conn{assigns: %{listing: listing, valid?: valid}} = conn
 
-    VolunteerWeb.Services.Analytics.track_event(
-      "Listing",
-      "admin_tkn_show",
-      Slugify.slugify(listing),
-      conn
-    )
-
-    case Listings.get_one_tkn_listing_for_listing(listing.id) do
-      nil ->
-        render(
-          conn,
-          "show_none.html",
-          listing: listing
-        )
-
-      tkn_listing ->
-        render(
-          conn,
-          "show_one.html",
-          listing: listing,
-          tkn_listing: tkn_listing
-        )
-    end
-  end
-
-  def new(conn, _params) do
-    %Plug.Conn{assigns: %{listing: listing}} = conn
-
-    VolunteerWeb.Services.Analytics.track_event(
-      "Listing",
-      "admin_tkn_new",
-      Slugify.slugify(listing),
-      conn
-    )
-
-    render_form(
+    render(
       conn,
-      Listings.new_tkn_listing(),
-      "new.html",
-      action_path: RouterHelpers.admin_listing_tkn_listing_path(conn, :create, listing)
+      "show.html",
+      listing: listing,
+      valid?: valid
     )
-  end
-
-  def create(conn, %{"tkn_listing" => tkn_listing_params}) do
-    %Plug.Conn{assigns: %{listing: listing}} = conn
-
-    listing
-    |> Listings.create_tkn_listing(tkn_listing_params)
-    |> case do
-      {:ok, tkn_listing} ->
-        VolunteerWeb.Services.Analytics.track_event(
-          "Listing",
-          "admin_tkn_create",
-          Slugify.slugify(listing),
-          conn
-        )
-
-        :ok = VolunteerWeb.Services.TKNAssignmentSpecGenerator.generate_async(conn, listing, tkn_listing)
-
-        conn
-        |> FlashHelpers.put_paragraph_flash(:success, "TKN Listing created successfully.")
-        |> redirect(to: RouterHelpers.admin_listing_tkn_listing_path(conn, :show, listing))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        conn
-        |> FlashHelpers.put_paragraph_flash(:error, "Oops, something went wrong! Please check the errors below.")
-        |> render_form(
-          changeset,
-          "new.html",
-          action_path: RouterHelpers.admin_listing_tkn_listing_path(conn, :create, listing)
-        )
-    end
   end
 
   def edit(conn, _params) do
-    %Plug.Conn{assigns: %{tkn_listing: tkn_listing, listing: listing}} = conn
+    %Plug.Conn{assigns: %{listing: listing}} = conn
 
-    changeset = Listings.edit_tkn_listing(tkn_listing)
-
-    VolunteerWeb.Services.Analytics.track_event(
-      "Listing",
-      "admin_tkn_edit",
-      Slugify.slugify(listing),
-      conn
-    )
+    changeset = Listings.TKN.edit(listing)
 
     render_form(
       conn,
       changeset,
       "edit.html",
-      tkn_listing: tkn_listing,
-      action_path: RouterHelpers.admin_listing_tkn_listing_path(conn, :update, listing)
+      listing: listing,
+      action_path: RouterHelpers.admin_listing_tkn_path(conn, :update, listing)
     )
   end
 
-  def update(conn, %{"tkn_listing" => tkn_listing_params}) do
-    %Plug.Conn{assigns: %{listing: listing, tkn_listing: tkn_listing}} = conn
+  def update(conn, %{"listing" => listing_params}) do
+    %Plug.Conn{assigns: %{listing: listing}} = conn
 
-    case Listings.update_tkn_listing(tkn_listing, tkn_listing_params) do
-      {:ok, tkn_listing} ->
-        VolunteerWeb.Services.Analytics.track_event(
-          "Listing",
-          "admin_tkn_update",
-          Slugify.slugify(listing),
-          conn
-        )
-
-        :ok = VolunteerWeb.Services.TKNAssignmentSpecGenerator.generate_async(conn, listing, tkn_listing)
+    case Listings.TKN.update(listing, listing_params) do
+      {:ok, listing} ->
+        :ok = VolunteerWeb.Services.TKNAssignmentSpecGenerator.generate_async(conn, listing)
 
         conn
         |> FlashHelpers.put_paragraph_flash(:success, "Listing updated successfully.")
-        |> redirect(to: RouterHelpers.admin_listing_tkn_listing_path(conn, :show, listing))
+        |> redirect(to: RouterHelpers.admin_listing_tkn_path(conn, :show, listing))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         conn
@@ -189,8 +80,8 @@ defmodule VolunteerWeb.Admin.TKNListingController do
         |> render_form(
           changeset,
           "edit.html",
-          tkn_listing: tkn_listing,
-          action_path: RouterHelpers.admin_listing_tkn_listing_path(conn, :update, listing)
+          listing: listing,
+          action_path: RouterHelpers.admin_listing_tkn_path(conn, :update, listing)
         )
     end
   end
@@ -205,15 +96,15 @@ defmodule VolunteerWeb.Admin.TKNListingController do
         [
           changeset: changeset,
           listing: conn.assigns[:listing],
-          back_path: RouterHelpers.admin_listing_tkn_listing_path(conn, :show, conn.assigns[:listing]),
-          classification_choices: Listings.TKNListing.classification_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          commitment_type_choices: Listings.TKNListing.commitment_type_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          location_type_choices: Listings.TKNListing.location_type_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          search_scope_choices: Listings.TKNListing.search_scope_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          function_choices: Listings.TKNListing.function_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          industry_choices: Listings.TKNListing.industry_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          education_level_choices: Listings.TKNListing.education_level_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
-          work_experience_years_choices: Listings.TKNListing.work_experience_years_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          back_path: RouterHelpers.admin_listing_tkn_path(conn, :show, conn.assigns[:listing]),
+          classification_choices: Listings.TKN.Change.classification_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          commitment_type_choices: Listings.TKN.Change.commitment_type_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          location_type_choices: Listings.TKN.Change.location_type_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          search_scope_choices: Listings.TKN.Change.search_scope_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          function_choices: Listings.TKN.Change.function_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          industry_choices: Listings.TKN.Change.industry_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          education_level_choices: Listings.TKN.Change.education_level_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
+          work_experience_years_choices: Listings.TKN.Change.work_experience_years_choices() |> VolunteerUtils.Choices.make(%{blank: true}),
         ]
     )
   end
